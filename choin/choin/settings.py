5e22e513e92b90ba68caa686d64679e61aec6c36
Eunice Atieno <eunniceatieno@gmail.com>
2021-10-15 16:03:12 +0300

    changed Settings.py file

--- choin/choin/settings.py
+++ choin/choin/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'choin/static')
 ]
 
 # Default primary key field type
