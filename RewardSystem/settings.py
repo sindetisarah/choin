d9f026310932ec14357108423832d7b8d8c4cb44
Eunice Atieno <eunniceatieno@gmail.com>
2021-10-17 13:43:55 +0300

    First Deploy

--- RewardSystem/settings.py
+++ RewardSystem/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'rewardsystem',
         'USER': 'Miners',
-        'PASSWORD': 'Miners$$04',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
     }
