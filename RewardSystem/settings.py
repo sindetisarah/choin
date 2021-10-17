9441283e628720176ca7d190d40406e09f924d9b
Eunice Atieno <eunniceatieno@gmail.com>
2021-10-17 14:13:11 +0300

    First Deploy

--- RewardSystem/settings.py
+++ RewardSystem/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'RewardSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'rewardsystem',
+#         'USER': 'TheMiners',
+#         'PASSWORD': 'miners',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rewardsystem',
-        'USER': 'Miners',
-        'PASSWORD': 'miners',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
