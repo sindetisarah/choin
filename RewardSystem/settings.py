60602b2709cc406fe9fedca178e167a0958a0fd2
Eunice Atieno <eunniceatieno@gmail.com>
2021-10-18 09:13:17 +0300

    first deploy

--- RewardSystem/settings.py
+++ RewardSystem/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'RewardSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rewardsystem',
-        'USER': 'TheMiners',
-        'PASSWORD': 'miners',
+        'NAME': 'choinproject',
+        'USER': 'minersgroup',
+        'PASSWORD': 'choinersminers',
         'HOST': 'localhost',
         'PORT': '',
     }
