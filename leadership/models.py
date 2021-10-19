f2401dae53763d655bf844915cd640a15116446c
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-10-19 18:44:27 +0300

    Added User Authentication

--- leadership/models.py
+++ leadership/models.py
@@ -1,15 +1,27 @@
 from django.db import models
-# Create your models here.
+from .manager import CustomUserManager
 from django.contrib.auth.models import AbstractUser,User
 import uuid
-from django.db.models.deletion import CASCADE
-from django.db.models.fields import CharField
+
 # Create your models here.
 class User(AbstractUser):
     user_name = models.CharField(('username'), unique=True,max_length=40),
     email = models.EmailField(('email address'), unique=True)
-    # password=models.CharField(('password'),max_length=100,default=uuid.uuid4)
+    is_superadmin = models.BooleanField(('is_superadmin'), default=False)
+    is_active = models.BooleanField(('is_active'), default=True)
+    is_staff = models.BooleanField(default=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = CustomUserManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    class Meta:
+        verbose_name = ('user')
+        verbose_name_plural = ('users')
+
+    def __str__(self):
+        """stirng representation"""
+        return self.email
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
