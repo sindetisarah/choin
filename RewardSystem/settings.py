95b70fd754d91124fc39f974bdd388d9144e9a06
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-10-19 07:53:35 +0300

    Added code to send emails containing paswwords to users

--- RewardSystem/settings.py
+++ RewardSystem/settings.py
@@ -125,6 +125,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -133,7 +135,13 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 AUTH_USER_MODEL = 'leadership.User'
-# AUTH_USER_MODEL_USERNAME_FIELD = 'username'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'choinminers@gmail.com'
+EMAIL_HOST_PASSWORD = 'akirachoinminers'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
