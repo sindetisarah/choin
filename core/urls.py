a6807251c511654074e0de76a06e807c66e93dd4
sindetisarah <sindetisara@gmail.com>
2021-10-20 13:46:30 +0300

    create navbar

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . views import  *
 urlpatterns=[
     path('',LoginView.as_view(),name='login'),
     path('home/',index,name='index'),
-    path('profile/',Profile,name='user_profile')
+    path('profile/',Profile,name='user_profile'),
+    path('navbar/',navbar,name='nav_bar')
+
     ]
