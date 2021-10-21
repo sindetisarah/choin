ed986801b77a68073d02d08213906e05d6e62d6f
sindetisarah <sindetisara@gmail.com>
2021-10-21 12:00:23 +0300

    email_password changed

--- RewardSystem/settings.py
+++ RewardSystem/settings.py
@@ -123,7 +123,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 
@@ -142,7 +142,7 @@ AUTH_USER_MODEL = 'leadership.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'choinminers@gmail.com'
-EMAIL_HOST_PASSWORD = 'akirachoinminers'
+EMAIL_HOST_PASSWORD = 'akirachixchoinminers123'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
--- leadership/urls.py
+++ leadership/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . views import profile_upload
 urlpatterns =[
     path('upload/',profile_upload,name='upload')
-    ]
+]
