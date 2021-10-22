f0768876809959913b2184dbe5eac086c85f8cee
Eunice Atieno <eunniceatieno@gmail.com>
2021-10-22 12:23:33 +0300

    db

--- core/views.py
+++ core/views.py
@@ -39,13 +39,7 @@ class LoginView(FormView):
             return HttpResponseRedirect(reverse_lazy('login'))
 
 
-# def index(request):
-# 	if request.method == "POST":
-# 		form = AuthenticationForm(request, data=request.POST)
-# 		if form.is_valid():
-# 			username = form.cleaned_data.get('username')
-# 			password = form.cleaned_data.get('password')
-# 			user = authenticate(username=username, password=password)
+	# user = authenticate(username=username, password=password)
 # 			if user is not None:
 # 				login(request, user)
 # 				messages.info(request, f"You are now logged in as {username}.")
