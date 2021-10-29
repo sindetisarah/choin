9eb3c88386a83ecea9bf8d704fc02a23603b2d66
chantal2000 <nniyonkuruchantal@gmail.com>
2021-10-29 15:07:25 +0300

    add close icon

--- student/urls.py
+++ student/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . views import redeem
+from core.views import Profile
 urlpatterns =[
-    path('redeem/',redeem,name='redeem')
+    path('redeem/',redeem,name='redeem'),
+    path('profile/',Profile,name='profile'),
+
 
     ]
