eb74c779f6424c25538ffb5e0fb280184a45ee2b
hunterachieng <achienghunter@gmail.com>
2021-11-12 11:42:09 +0300

    redeem popups

--- student/views.py
+++ student/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.core.paginator import Paginator
@@ -10,22 +11,17 @@ import json
 from .models import  *
 from django.core.exceptions import ObjectDoesNotExist
 from leadership.models import RedeemableItem, Transaction, Wallet
-
-
-# @login_required
-
-
-
+from django.contrib.auth.decorators import login_required
+from leadership.views import view_student_leaderboard
+@login_required(login_url='login')
 def student_profile(request):
     try:
         userprofile = request.user.userprofile
     except ObjectDoesNotExist:
         userprofile = Student(user=request.user)
-        
     if request.method == 'POST':
         user_form = UpdateProfileForm(request.POST, request.FILES,instance=request.user)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile.user)
-
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save(commit=False)
             #user.username = user.email
@@ -33,7 +29,7 @@ def student_profile(request):
             profile = profile_form.save(commit=False)
             profile.user = user
             profile.save()
-            return redirect('student-home')
+            return redirect('student_dashboard')
     else:
         profile_form = UserProfileForm(instance=request.user.userprofile)
         user_form = UpdateProfileForm(instance=request.user)
@@ -42,11 +38,9 @@ def student_profile(request):
         'profile_form': profile_form # user profile form
         }
     return render(request, 'student_profile.html', args)
-
 def student_home(request):
     return render(request,'student_home.html')
-
-    
+@login_required(login_url='login')
 def redeem(request):
     if request.user.is_authenticated:
         student_customer = Student.objects.get(user = request.user)
@@ -64,12 +58,11 @@ def redeem(request):
         if reward.activate_page == False:
             return render(request,'inactive_redeem.html',{'bal':bal, 'items':items})
     return render(request,'redeem.html',{'reward_items':reward_items,'bal':bal, 'items':items, 'cartItems':cartItems})
-    
-
+@login_required(login_url='login')
 def redeem_failed(request):
     bal = Wallet.objects.all().filter(owner = request.user)
     return render(request,'RedeemFailed.html',{'bal':bal})
-
+@login_required(login_url='login')
 def redeem_success(request):
     if request.user.is_authenticated:
         student_customer = Student.objects.get(user = request.user)
@@ -78,47 +71,41 @@ def redeem_success(request):
         print(items)
     else:
         items= []
-    context = {'items':items, 'order':order}  
+    context = {'items':items, 'order':order}
     return render(request,'RedeemSucceed.html',context)
-
+@login_required(login_url='login')
 def cart(request):
     bal = Wallet.objects.all().filter(owner = request.user)
     if request.user.is_authenticated:
         student_customer = Student.objects.get(user = request.user)
         # student_customer = request.user.role==3
-        
         order, created = Redeem.objects.get_or_create(student=student_customer, complete = False)
         items = order.rewardeditem_set.all()
-        
         # cartItems = order.calculate_cart_items()
     else:
         items= []
-
-
         order={'calculate_cart_total':0, 'calculate_cart_items':0}
-    
-
-    context = {'items':items, 'order':order,'bal':bal}    
+    context = {'items':items, 'order':order,'bal':bal}
     return render(request,'cart.html', context)
-    
-
+@login_required(login_url='login')
 def redeem_active(request):
     return render(request,'redeem_active.html')
-
+@login_required(login_url='login')
 def student_dashboard(request):
     student=Student.objects.get(user=request.user)
-    choin_balance=Wallet.objects.get(owner=request.user)
-    data={'student':student,'choin_balance':choin_balance}
-    return render(request,'stud_dashboard.html')
-
+    transactions = Transaction.objects.all().filter(receiver = request.user.username)
+    students=Wallet.objects.all().order_by('-choinBalance')
+    choin_balance=Wallet.objects.all().filter(owner=request.user)
+    data={'student':student,'choin_balance':choin_balance,'students':students,'transactions':transactions}
+    return render(request,'stud_dashboard.html',data)
+@login_required(login_url='login')
 def student_transactions(request):
     transact = Transaction.objects.all().filter(receiver = request.user.username)
     bal = Wallet.objects.all().filter(owner = request.user)
     paginator = Paginator(transact, 5)
     page = request.GET.get('page')
     transactions = paginator.get_page(page)
-    return render(request,'student_transactions.html',{'transactions':transactions,'bal':bal})  
-
+    return render(request,'student_transactions.html',{'transactions':transactions,'bal':bal})
 # def view_redeemed_items(request):
 #     if request.user.is_authenticated:
 #         customer=request.user.idt
@@ -126,35 +113,27 @@ def student_transactions(request):
 #         items=order.orderedproduct_set.all()
 #     else:
 #         items=[]
-
-#     return render(request,'student_transactions.html',{'transactions':transactions,'bal':bal})  
-
+#     return render(request,'student_transactions.html',{'transactions':transactions,'bal':bal})
 def update_item(request):
     data = json.loads(request.body)
-    productId = data['productId'] 
+    productId = data['productId']
     action = data['action']
-
     print('Action:', action)
-    print('ProductId:', productId) 
-
+    print('ProductId:', productId)
     student_customer = Student.objects.get(user = request.user)
     product = RedeemableItem.objects.get(id=productId)
     order, created = Redeem.objects.get_or_create(student=student_customer, complete = False)
     orderItem, created = RewardedItem.objects.get_or_create(order = order, reward=product )
-
     if action =='add':
         orderItem.quantity = (orderItem.quantity + 1)
     elif action == 'remove':
          orderItem.quantity = (orderItem.quantity - 1)
-    
     orderItem.student = Student.objects.get(user=request.user)
     orderItem.save()
-
     if orderItem.quantity <= 0:
        orderItem.delete()
-
     return JsonResponse('Item was added', safe=False)
-
+@login_required(login_url='login')
 def student_redeem(request):
     bal = Wallet.objects.all().filter(owner = request.user)
     std = Student.objects.get(user = request.user)
@@ -162,38 +141,12 @@ def student_redeem(request):
     the_balance=None
     for b in bal:
         for ord in order:
-
             if b.choinBalance < ord.calculate_cart_total:
                 return redirect('redeem_failed')
             else:
-     
                 wallets=Wallet.objects.all().filter(owner=request.user)
                 the_balance =b.choinBalance - ord.calculate_cart_total
                 red = Redeem.objects.all().filter(student = std)
                 red.delete()
-                
-                
                 wallets.update(owner = request.user, choinBalance = the_balance)
-
-    return render(request,'RedeemSucceed.html',{'the_balance':the_balance,'order':order})        
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-# def mark_as_read(request,pk):
-    
-#     notifications = Notifications.objects.get(pk=pk)
-#     notifications.mark_as_read()
-#     return redirect(reverse('student-home'))
-
-
+    return render(request,'RedeemSucceed.html',{'the_balance':the_balance,'order':order})
